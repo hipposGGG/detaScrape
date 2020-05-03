2efcb1f455b746a7d9dc960e10f5503185ec65dd
高橋周 <ebitabetai7@gmail.com>
2020-05-04 00:56:03 +0900

    for a in soup.find_all("div",{"class_":"rg_meta"}): 　処理詳細追えていない

--- scrape.py
+++ scrape.py
@@ -12,6 +12,7 @@ import argparse
 import sys
 import json
 import codecs
+import logging
 
 
 # How To
@@ -21,25 +22,30 @@ import codecs
 # -s: Google Imagesにかける検索キーワード、複数可 (デフォルト "banana")
 # -n: ダウンロードする画像の数量 (デフォルト 10枚)
 # -o: 画像の保存先 (デフォルト　<DEFAULT_SAVE_DIRECTORY>で指定する)
+logging.basicConfig(filename='D:\eclipse-workspace\python_test\logfile\logger.log', level=logging.INFO)
 
 def get_soup(url,header):
     return bs4.BeautifulSoup(urllib.request.urlopen(urllib.request.Request(url,headers=header)),'html.parser')
 
 def main(args):
+    DEFAULT_SAVE_DIRECTORY = 'D:\eclipse-workspace\python_test\scrape'
     parser = argparse.ArgumentParser(description='Options for scraping Google images')
     parser.add_argument('-s', '--search', default='banana', type=str, help='search term')
     parser.add_argument('-n', '--num_images', default=10, type=int, help='num of images to scrape')
-    parser.add_argument('-o', '--directory', default='<DEFAULT_SAVE_DIRECTORY>', type=str, help='output directory')
+    parser.add_argument('-o', '--directory', default=DEFAULT_SAVE_DIRECTORY, type=str, help='output directory')
     args = parser.parse_args()
 
     # 複数のキーワードを"+"で繋げる
     query = args.search.split()
+    logging.info("1回目： %s",query)
     query = '+'.join(query)
+    logging.info("2回目： %s",query)
     max_images = args.num_images
-#     max_images = 10
+    logging.info("max_images： %s",max_images)
 
     # 画像をフォルダーでグループする
-    save_directory = args.directory + '/' + query
+    save_directory = args.directory + '\\' + query
+    logging.info("save_directory： %s",save_directory)
     if not os.path.exists(save_directory):
         os.makedirs(save_directory)
 #     save_directory = "D:\eclipse-workspace\python_test\scrape\cat"
@@ -49,17 +55,22 @@ def main(args):
     # スクレーピング Chrome/43.0.2357.134
     url="https://www.google.co.jp/search?q="+urllib.parse.quote(query)+"&source=lnms&tbm=isch"
     header={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36"}
+    logging.info("url： %s",url)
+    logging.info("header： %s",header)
     soup = get_soup(url,header)
     ActualImages=[]
-#     print("★Fsoup:",Fsoup)
-#     soup = soupTmp.decode('utf-8')
+#     logging.info("souptmp： %s",souptmp)
 #     print("★soup:",soup)
-    soup = soup.decode('utf-8')
+#     soup = souptmp.decode('utf-8')
+#     logging.info("soup： %s",soup)
+    print("★soup★",soup)
 
 #     for a in (soup.decode('utf-8')).find_all("div",{"class":"rg_meta"}):
-#     for a in soup.find_all("div",{"class_":"rg_meta"}):
-    for a in soup.find_all("div"):
-        print("★a:",a)
+    #ここから処理詳細追えていない
+    #↓↓↓↓↓↓↓↓↓↓↓↓↓
+    # 要素名が div でclass_またはrg_metaを含む要素を抽出
+    for a in soup.find_all("div",{"class_":"rg_meta"}):
+        logging.info("a： %s",a)
         link , Type =json.loads(a.text)["ou"]  ,json.loads(a.text)["ity"]
         ActualImages.append((link,Type))
     for i , (img , Type) in enumerate( ActualImages[0:max_images]):
